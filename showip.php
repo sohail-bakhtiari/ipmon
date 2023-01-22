14b187fd2da296e2f3f4231f3a435c5ffd0bb394
Sohail <sbakhtiari@gmail.com>
2023-01-22 14:34:13 +0100

    Comma Separated List

--- showip.php
+++ showip.php
@@ -1,23 +1,20 @@
 <?php
 
-if(isset($_GET["tz"]))
+if (isset($_GET["tz"]))
 	date_default_timezone_set($_GET["tz"]);
 else
 	date_default_timezone_set("Europe/Berlin");
-	//date_default_timezone_set("Asia/Tehran");
+//date_default_timezone_set("Asia/Tehran");
 
-
-$dt = date("D Y-m-d H:i:s");
+$dt = date("Y-m-d H:i:s");
 $ip = $_SERVER['REMOTE_ADDR'];
 
-if(isset($_GET["ret"]))
-{
-	switch($_GET["ret"])
-	{
+if (isset($_GET["ret"])) {
+	switch ($_GET["ret"]) {
 		case "ip":
 			print($ip);
 			break;
-		
+
 		case "time":
 			print($dt);
 			break;
@@ -26,41 +23,33 @@ if(isset($_GET["ret"]))
 			print("$dt - $ip");
 			break;
 		case "json":
-			$dshort = date("Y-m-d");
-			$tshort = date("H:i:s");
-			print("{\"date\":\"$dshort\", \"time\":\"$tshort\", \"ip\":\"$ip\"}");
+			print("{\"date\":\"$d\", \"ip\":\"$ip\"}");
 			break;
 	}
-} 
-else 
-{
+} else {
 	print($ip);
 }
 
-try 
-{
-	$filename = $_GET["n"] . "_ip.txt";
-	$fp = fopen($filename, "w");
+$name = $_GET["n"];
+$data = "";
+if (isset($_GET["data"]))
+	$data = ", " . $_GET["data"];
+
+try {
+	$fp = fopen($name . "_ip.txt", "w");
 	fwrite($fp, $ip);
-	if(isset($_GET["data"]))
+	if (isset($_GET["data"]))
 		fwrite($fp, " : " . $_GET["data"]);
 	fclose($fp);
 
-	$filename = $_GET["n"] . "_log.txt";
-
-	if(file_exists($filename) && filesize($filename) > 50000)
-	{
-		file_put_contents(substr($filename, 0, -3) . "bak", file_get_contents($filename),  FILE_APPEND);
-		unlink($filename);
+	$logfile = $name . "_log.txt";
+	if (file_exists($logfile) && filesize($logfile) > 50000) {
+		file_put_contents(substr($logfile, 0, -3) . "bak", file_get_contents($logfile),  FILE_APPEND);
+		unlink($logfile);
 	}
 
-	$fp = fopen($filename, "a");
-	fwrite($fp, "$dt -> $ip");
-	
-	if(isset($_GET["data"]))
-		fwrite($fp, " : " . $_GET["data"]);
-	
-	fwrite($fp, "\r\n");
+	$fp = fopen($logfile, "a");
+	fwrite($fp, "$dt, $ip$data\r\n");
 	fclose($fp);
 	
 } catch (Exception $e) {
